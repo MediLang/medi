0e0fd7a19515f82dd2fbbf214cf6bd2f98f78d2e
Kresna Sucandra <kresnasucandra@gmail.com>
2025-05-10 21:05:56 +0800

    Add loop, match, return statements and bool/float literals to parser and AST; improve error handling and tests

--- src/ast.rs
+++ src/ast.rs
@@ -51,4 +51,65 @@ pub struct HealthcareQueryNode {
     pub arguments: Vec<ExpressionNode>,
 }
 
+#[derive(Debug, Clone, PartialEq)]
+pub enum StatementNode {
+    Let(Box<LetStatementNode>),
+    Assignment(Box<AssignmentNode>),
+    Expr(ExpressionNode),
+    Block(BlockNode),
+    If(Box<IfNode>),
+    While(Box<WhileNode>),
+    For(Box<ForNode>),
+    Match(Box<MatchNode>),
+    Return(Box<ReturnNode>),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct LetStatementNode {
+    pub name: String,
+    pub value: ExpressionNode,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct AssignmentNode {
+    pub target: ExpressionNode, // Usually Identifier or Member
+    pub value: ExpressionNode,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct BlockNode {
+    pub statements: Vec<StatementNode>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct IfNode {
+    pub condition: ExpressionNode,
+    pub then_branch: BlockNode,
+    pub else_branch: Option<BlockNode>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct WhileNode {
+    pub condition: ExpressionNode,
+    pub body: BlockNode,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ForNode {
+    pub var: String,
+    pub iter: ExpressionNode,
+    pub body: BlockNode,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct MatchNode {
+    pub expr: ExpressionNode,
+    pub arms: Vec<(ExpressionNode, BlockNode)>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ReturnNode {
+    pub value: Option<ExpressionNode>,
+}
+
 // Add more AST nodes as needed (statements, declarations, etc.)
